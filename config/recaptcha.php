538bfb64f5fd1efa41990c91e9d3e9521c3b91d2
Dick van der Heiden <dick@goedemiddag.nl>
2024-05-24 20:42:00 +0200

    📝 Improve documentation

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -1,15 +1,14 @@
 <?php
 
 return [
-
     'site_key' => env('RECAPTCHA_SITE_KEY'),
-
     'secret_key' => env('RECAPTCHA_SECRET_KEY'),
 
+    // The threshold to pass the recaptcha validation, from 0 (easiest) to 1 (hardest)
     'threshold' => env('RECAPTCHA_THRESHOLD', 0.3),
 
+    // Provide IP addresses that shouldn't be validated
     'skip_ips' => [
         // 127.0.0.1
     ],
-
 ];
